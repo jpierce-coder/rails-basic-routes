93a8c7f3330cc59a214474237b59fa15c4fc021a
John P <70922864+jpierce-coder@users.noreply.github.com>
2023-02-01 23:23:07 -0600

    Edit Root Route in App

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # localhost:3000/
+  # THREE WAYS TO SET ROOT PATH:
+  # root "pages#landing"
+  # root to: "pages#landing"
+  # get "/", to: "pages#landing"
 end
